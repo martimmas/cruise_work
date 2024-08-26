d2f663bf2ffa87a647d544717be130cdf6e7e4c4
Martim Mas e Braga <mmeb1@st-andrews.ac.uk>
2024-08-26 14:54:08 +0100

    fixed bug in discarding outliers; changed outlier thresholds

--- check_offsets_calibration_salt.m
+++ check_offsets_calibration_salt.m
@@ -2,26 +2,38 @@
 std_threshold            = 0.0025;
 threshold_halocline_grad = 0.1;
 min_depth_threshold      = 200;
+stn_salt = stn;
 
 % an outlier has a standard deviation above <std_threshold> and is not in
 % the halocline, considered where |grad(conductivity)| > <threshold_halocline>
 i_outliers1 = (stdc1 > std_threshold | abs(ctdgradc1) > threshold_halocline_grad);% & botpress < min_depth_threshold;
 i_outliers2 = (stdc2 > std_threshold | abs(ctdgradc2) > threshold_halocline_grad);% & botpress < min_depth_threshold;
 
-condoff1_filtered  = condoff1(~i_outliers1);
-condoff2_filtered  = condoff2(~i_outliers2);
-stdc1_filtered     = stdc1(~i_outliers1);
-stdc2_filtered     = stdc2(~i_outliers2);
-botpress_filtered1 = botpress(~i_outliers1);
-botpress_filtered2 = botpress(~i_outliers2);
-botc_filtered1     = botc(~i_outliers1);
-botc_filtered2     = botc(~i_outliers2);
-ctdc1_filtered     = ctdc1(~i_outliers1);
-ctdc2_filtered     = ctdc2(~i_outliers2);
-ctds1_filtered     = ctds1(~i_outliers1);
-ctds2_filtered     = ctds2(~i_outliers2);
+ctdc1_filtered     = ctdc1;
+ctdc2_filtered     = ctdc2;
+ctds1_filtered     = ctds1;
+ctds2_filtered     = ctds2;
+condoff1_filtered  = condoff1;
+condoff2_filtered  = condoff2;
+stdc1_filtered     = stdc1;
+stdc2_filtered     = stdc2;
+botpress_filtered1 = botpress;
+botpress_filtered2 = botpress;
+botc_filtered1     = botc;
+botc_filtered2     = botc;
+condoff1_filtered(i_outliers1)  = NaN;
+condoff2_filtered(i_outliers2)  = NaN;
+stdc1_filtered(i_outliers1)     = NaN;
+stdc2_filtered(i_outliers2)     = NaN;
+botpress_filtered1(i_outliers1) = NaN;
+botpress_filtered2(i_outliers2) = NaN;
+botc_filtered1(i_outliers1)     = NaN;
+botc_filtered2(i_outliers2)     = NaN;
+ctdc1_filtered(i_outliers1)     = NaN;
+ctdc2_filtered(i_outliers2)     = NaN;
+ctds1_filtered(i_outliers1)     = NaN;
+ctds2_filtered(i_outliers2)     = NaN;
 
-%% Determining the offsets
 % RMSE of the raw data (for reference)
 rmse1_pre = rmse(ctdc1,botc,[1,2],'omitnan');
 rmse2_pre = rmse(ctdc2,botc,[1,2],'omitnan');
@@ -30,53 +42,46 @@ rmse2_pre = rmse(ctdc2,botc,[1,2],'omitnan');
 rmse1_filt = rmse(ctdc1(~i_outliers1),botc(~i_outliers1),[1,2],'omitnan');
 rmse2_filt = rmse(ctdc2(~i_outliers2),botc(~i_outliers2),[1,2],'omitnan');
 
+
+%% Determining the offsets
+offset_cond1 = botc_filtered1 - ctdc1_filtered;
+offset_cond2 = botc_filtered1 - ctdc2_filtered;
+
 % Look for the offset that yields the smallest RMSE
-depth_intervals = 0:10:800;
-offsets_by_depth = NaN([length(depth_intervals),2]);
-for k=1:length(depth_intervals)-1
-    i_depths1 = find(botpress_filtered1 >= depth_intervals(k) & botpress_filtered1 < depth_intervals(k+1));
-    ctdc1_filtered_depth = ctdc1_filtered(i_depths1);
-    botc1_filtered_depth = botc_filtered1(i_depths1);
-    rmse_best_estimate = 999;
-    for offset_estimate=-0.05:0.0001:0.05
-        rmse1_estimate = rmse(ctdc1_filtered_depth+offset_estimate,botc1_filtered_depth,'omitnan');
-        if rmse1_estimate < rmse_best_estimate 
-            offsets_by_depth(k,1) = offset_estimate; 
-            rmse_best_estimate = rmse1_estimate;
-        end
-    end
-    i_depths2 = find(botpress_filtered2 >= depth_intervals(k) & botpress_filtered2 < depth_intervals(k+1));
-    ctdc2_filtered_depth = ctdc2_filtered(i_depths2);
-    botc2_filtered_depth = botc_filtered2(i_depths2);
-    rmse_best_estimate = 999;
-    for offset_estimate=-0.05:0.0001:0.05
-        rmse2_estimate = rmse(ctdc2_filtered_depth+offset_estimate,botc2_filtered_depth,'omitnan');
-        if rmse2_estimate < rmse_best_estimate 
-            offsets_by_depth(k,2) = offset_estimate; 
-            rmse_best_estimate = rmse2_estimate;
-        end
-    end
-end
+% depth_intervals = 0:10:800;
+% offsets_by_depth = NaN([length(depth_intervals),2]);
+% for k=1:length(depth_intervals)-1
+%     i_depths1 = find(botpress_filtered1 >= depth_intervals(k) & botpress_filtered1 < depth_intervals(k+1));
+%     ctdc1_filtered_depth = ctdc1_filtered(i_depths1);
+%     botc1_filtered_depth = botc_filtered1(i_depths1);
+%     rmse_best_estimate = 999;
+%     for offset_estimate=-0.05:0.0001:0.05
+%         rmse1_estimate = rmse(ctdc1_filtered_depth+offset_estimate,botc1_filtered_depth,'omitnan');
+%         if rmse1_estimate < rmse_best_estimate 
+%             offsets_by_depth(k,1) = offset_estimate; 
+%             rmse_best_estimate = rmse1_estimate;
+%         end
+%     end
+%     i_depths2 = find(botpress_filtered2 >= depth_intervals(k) & botpress_filtered2 < depth_intervals(k+1));
+%     ctdc2_filtered_depth = ctdc2_filtered(i_depths2);
+%     botc2_filtered_depth = botc_filtered2(i_depths2);
+%     rmse_best_estimate = 999;
+%     for offset_estimate=-0.05:0.0001:0.05
+%         rmse2_estimate = rmse(ctdc2_filtered_depth+offset_estimate,botc2_filtered_depth,'omitnan');
+%         if rmse2_estimate < rmse_best_estimate 
+%             offsets_by_depth(k,2) = offset_estimate; 
+%             rmse_best_estimate = rmse2_estimate;
+%         end
+%     end
+% end
 % figure; scatter(offsets_by_depth,-depth_intervals) % sanity check plot
 %% Applying the offset
-ctdc1_corrected = ctdc1_filtered;
-ctdc2_corrected = ctdc2_filtered;
-
-% applying by depth
-% for k_bot=1:length(botpress_filtered1)    
-%     [~,k_depth] = min(abs(botpress_filtered1(k_bot)-depth_intervals)); % finds closest offset depth to the bottle depth
-%     ctdc1_corrected(k_bot) = ctdc1_corrected(k_bot)+offsets_by_depth(k_depth,1);
-% end
-% for k_bot=1:length(botpress_filtered2)    
-%     [~,k_depth] = min(abs(botpress_filtered2(k_bot)-depth_intervals)); % finds closest offset depth to the bottle depth   
-%     ctdc2_corrected(k_bot) = ctdc2_corrected(k_bot)+offsets_by_depth(k_depth,2);
-% end
 
 % applying single value for all
-offset1 = median(offsets_by_depth(:,1),'omitnan');
-offset2 = median(offsets_by_depth(:,2),'omitnan');
-ctdc1_corrected = ctdc1_corrected+offset1;
-ctdc2_corrected = ctdc2_corrected+offset2;
+offset1_cond = median(offset_cond1(:),'omitnan');
+offset2_cond = median(offset_cond2(:),'omitnan');
+ctdc1_corrected = ctdc1_filtered+offset1_cond;
+ctdc2_corrected = ctdc2_filtered+offset2_cond;
 %% Assessing the fit
 
 correl1_filt = corrcoef(botc_filtered1(~isnan(botc_filtered1)),ctdc1_filtered(~isnan(botc_filtered1)));
--- check_offsets_calibration_temp.m
+++ check_offsets_calibration_temp.m
@@ -1,19 +1,26 @@
 %% Filtering outliers
 std_threshold            = 0.0025;
-threshold_thermocline_grad = 0.1;
+threshold_thermocline_grad = 0.15;
 min_depth_threshold      = 200;
+stn_temp = stn;
 
 % an outlier has a standard deviation above <std_threshold> and is not in
 % the halocline, considered where |grad(conductivity)| > <threshold_halocline>
-i_outliers1 = abs(ctdgradt1) > threshold_thermocline_grad;% & botpress < min_depth_threshold;
-i_outliers2 = abs(ctdgradt2) > threshold_thermocline_grad;% & botpress < min_depth_threshold;
+i_outliers1 = (stdt1 > std_threshold | abs(ctdgradt1) > threshold_thermocline_grad);% & botpress < min_depth_threshold;
+i_outliers2 = (stdt2 > std_threshold | abs(ctdgradt2) > threshold_thermocline_grad);% & botpress < min_depth_threshold;
 
-botp_filtered1 = botp(~i_outliers1);
-botp_filtered2 = botp(~i_outliers2);
-sb35temp1      = sb35temp(~i_outliers1);
-sb35temp2      = sb35temp(~i_outliers2);
-ctdt1_filtered = ctdt1(~i_outliers1);
-ctdt2_filtered = ctdt2(~i_outliers2);
+botp_filtered1 = botp;
+botp_filtered2 = botp;
+sb35temp1      = sb35temp;
+sb35temp2      = sb35temp;
+ctdt1_filtered = ctdt1;
+ctdt2_filtered = ctdt2;
+botp_filtered1(i_outliers1) = NaN;
+botp_filtered2(i_outliers2) = NaN;
+sb35temp1(i_outliers1)      = NaN;
+sb35temp2(i_outliers2)      = NaN;
+ctdt1_filtered(i_outliers1) = NaN;
+ctdt2_filtered(i_outliers2) = NaN;
 
 %% Determining the offsets
 % RMSE of the raw data (for reference)
@@ -24,53 +31,44 @@ rmse2_pre = rmse(ctdt2,sb35temp,[1,2],'omitnan');
 rmse1_filt = rmse(ctdt1(~i_outliers1),sb35temp(~i_outliers1),[1,2],'omitnan');
 rmse2_filt = rmse(ctdt2(~i_outliers2),sb35temp(~i_outliers2),[1,2],'omitnan');
 
-% Look for the offset that yields the smallest RMSE
-depth_intervals = 0:10:800;
-offsets_by_depth = NaN([length(depth_intervals),2]);
-for k=1:length(depth_intervals)-1
-    i_depths1 = find(botp_filtered1 >= depth_intervals(k) & botp_filtered1 < depth_intervals(k+1));
-    ctdt1_filtered_depth = ctdt1_filtered(i_depths1);
-    sb35temp1_filtered_depth = sb35temp1(i_depths1);
-    rmse_best_estimate = 999;
-    for offset_estimate=-0.05:0.0001:0.05
-        rmse1_estimate = rmse(ctdt1_filtered_depth+offset_estimate,sb35temp1_filtered_depth,'omitnan');
-        if rmse1_estimate < rmse_best_estimate 
-            offsets_by_depth(k,1) = offset_estimate; 
-            rmse_best_estimate = rmse1_estimate;
-        end
-    end
-    i_depths2 = find(botp_filtered2 >= depth_intervals(k) & botp_filtered2 < depth_intervals(k+1));
-    ctdt2_filtered_depth = ctdt2_filtered(i_depths2);
-    sb35temp2_filtered_depth = sb35temp2(i_depths2);
-    rmse_best_estimate = 999;
-    for offset_estimate=-0.05:0.0001:0.05
-        rmse2_estimate = rmse(ctdt2_filtered_depth+offset_estimate,sb35temp2_filtered_depth,'omitnan');
-        if rmse2_estimate < rmse_best_estimate 
-            offsets_by_depth(k,2) = offset_estimate; 
-            rmse_best_estimate = rmse2_estimate;
-        end
-    end
-end
-% figure; scatter(offsets_by_depth,-depth_intervals) % sanity check plot
-%% Applying the offset
-ctdt1_corrected = ctdt1_filtered;
-ctdt2_corrected = ctdt2_filtered;
+offset_temp1 = sb35temp1 - ctdt1_filtered;
+offset_temp2 = sb35temp2 - ctdt2_filtered;
 
-% applying by depth
-% for k_bot=1:length(botpress_filtered1)    
-%     [~,k_depth] = min(abs(botpress_filtered1(k_bot)-depth_intervals)); % finds closest offset depth to the bottle depth
-%     ctdt1_corrected(k_bot) = ctdt1_corrected(k_bot)+offsets_by_depth(k_depth,1);
+% % Look for the offset that yields the smallest RMSE
+% depth_intervals = 0:10:800;
+% offsets_by_depth = NaN([length(depth_intervals),2]);
+% for k=1:length(depth_intervals)-1
+%     i_depths1 = find(botp_filtered1 >= depth_intervals(k) & botp_filtered1 < depth_intervals(k+1));
+%     ctdt1_filtered_depth = ctdt1_filtered(i_depths1);
+%     sb35temp1_filtered_depth = sb35temp1(i_depths1);
+%     rmse_best_estimate = 999;
+%     for offset_estimate=-0.05:0.0001:0.05
+%         rmse1_estimate = rmse(ctdt1_filtered_depth+offset_estimate,sb35temp1_filtered_depth,'omitnan');
+%         if rmse1_estimate < rmse_best_estimate 
+%             offsets_by_depth(k,1) = offset_estimate; 
+%             rmse_best_estimate = rmse1_estimate;
+%         end
+%     end
+%     i_depths2 = find(botp_filtered2 >= depth_intervals(k) & botp_filtered2 < depth_intervals(k+1));
+%     ctdt2_filtered_depth = ctdt2_filtered(i_depths2);
+%     sb35temp2_filtered_depth = sb35temp2(i_depths2);
+%     rmse_best_estimate = 999;
+%     for offset_estimate=-0.05:0.0001:0.05
+%         rmse2_estimate = rmse(ctdt2_filtered_depth+offset_estimate,sb35temp2_filtered_depth,'omitnan');
+%         if rmse2_estimate < rmse_best_estimate 
+%             offsets_by_depth(k,2) = offset_estimate; 
+%             rmse_best_estimate = rmse2_estimate;
+%         end
+%     end
 % end
-% for k_bot=1:length(botpress_filtered2)    
-%     [~,k_depth] = min(abs(botpress_filtered2(k_bot)-depth_intervals)); % finds closest offset depth to the bottle depth   
-%     ctdt2_corrected(k_bot) = ctdt2_corrected(k_bot)+offsets_by_depth(k_depth,2);
-% end
+% % figure; scatter(offsets_by_depth,-depth_intervals) % sanity check plot
+%% Applying the offset
 
 % applying single value for all
-offset1_temp = median(offsets_by_depth(:,1),'omitnan');
-offset2_temp = median(offsets_by_depth(:,2),'omitnan');
-ctdt1_corrected = ctdt1_corrected+offset1_temp;
-ctdt2_corrected = ctdt2_corrected+offset2_temp;
+offset1_temp = median(offset_temp1(:),'omitnan');
+offset2_temp = median(offset_temp2(:),'omitnan');
+ctdt1_corrected = ctdt1_filtered+offset1_temp;
+ctdt2_corrected = ctdt2_filtered+offset2_temp;
 %% Assessing the fit
 
 correl1_filt = corrcoef(sb35temp1(~isnan(sb35temp1)),ctdt1_filtered(~isnan(sb35temp1)));
